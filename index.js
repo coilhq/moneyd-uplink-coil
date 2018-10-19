815a5c7ba21948104715ac40d653595bc8a6ad51
sharafian <ben@sharafian.me>
2018-10-19 15:50:55 -0400

    fix: better error handling

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ async function configure ({ advanced }) {
     }
   }
 
-  let response
   try {
-    response = await axios({
+    const response = await axios({
       method: 'POST',
       url: 'https://coil.com/graphql',
       data: {
@@ -38,30 +37,30 @@ async function configure ({ advanced }) {
         query: "mutation Login($email: String!, $password: String!) {\n  login(email: $email, password: $password) {\n    token\n    user {\n      ...CurrentUser\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment CurrentUser on User {\n  id\n  email\n  fullName\n  shortName\n  subscription {\n    stripeId\n    plan {\n      stripeId\n      tier\n      cost\n      name\n      __typename\n    }\n    active\n    endDate\n    autoRenew\n    __typename\n  }\n  customerId\n  invitation {\n    code\n    __typename\n  }\n  paymentPointer\n  emailVerify {\n    id\n    verified\n    __typename\n  }\n  intercomHmac\n  currencyPreferences {\n    code\n    scale\n    __typename\n  }\n  trial\n  __typename\n}\n"
       }
     })
+
+    const currency = response.data.data.login.user.currencyPreferences
+    const assetCode = currency ? currency.code : 'USD'
+    const assetScale = currency ? currency.scale : 9
+
+    console.log('logged in successfully.')
+    console.log('run `moneyd coil:start` to connect to Interledger.')
+    return {
+      relation: 'parent',
+      plugin: require.resolve('ilp-plugin-btp-coil'),
+      assetCode,
+      assetScale,
+      sendRoutes: false,
+      receiveRoutes: false,
+      options: {
+        assetScale: 9,
+        coilToken: response.data.data.login.token,
+        server
+      }
+    }
   } catch (e) {
     console.error('Error: invalid username or password.') 
     debug('error logging in:', e)
     process.exit(1)
-  }
-
-  const currency = response.data.data.login.user.currencyPreferences
-  const assetCode = currency ? currency.code : 'USD'
-  const assetScale = currency ? currency.scale : 9
-
-  console.log('logged in successfully.')
-  console.log('run `moneyd coil:start` to connect to Interledger.')
-  return {
-    relation: 'parent',
-    plugin: require.resolve('ilp-plugin-btp-coil'),
-    assetCode,
-    assetScale,
-    sendRoutes: false,
-    receiveRoutes: false,
-    options: {
-      assetScale: 9,
-      coilToken: response.data.data.login.token,
-      server
-    }
   }
 }
 
